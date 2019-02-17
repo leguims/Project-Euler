235bf281571960a361ab7a4f25979d8bec80bda3
leguims <leguims@free.fr>
2019-02-17 22:00:10 +0100

    Improve solution 6

--- Problem0006.py
+++ Problem0006.py
@@ -19,14 +19,34 @@ def main():
 
     max = 100 #10
 
+    print(f" * Brute force implementation : ")
     sumOfSquare = sum([pow(i, 2) for i in range(1, max +1)])
     squareOfSum = pow(sum(range(1, max +1)), 2)
-    Solution = difference = squareOfSum - sumOfSquare
+    difference = squareOfSum - sumOfSquare
     print(f"Sum Of Square of first {max} natural = {sumOfSquare}")
     print(f"Square Of Sum of first {max} natural = {squareOfSum}")
+    print(f"Difference = {difference}")
+
+    print("""
+ * Smart implementation : 
+Square Of Sum = (1+2+3...+N)^2 = 1^2+2^2+3^2+...+N^2+ 2x(1x2 + 1x3 +...+ 1xN + 2x3 + ... + 2xN + 3x4 + ... + 3xN + ... + (N-1)xN)
+Sum Of Square = 1^2+2^2+3^2+...+N^2
+Square Of Sum - Sum Of Square = 2x(1x2 + 1x3 +...+ 1xN + 2x3 +...+ 2xN + 3x4 +...+ 3xN +...+ (N-1)xN)
+Square Of Sum - Sum Of Square = 2x( SUM[M=1;N-1](M x SUM[S=M+1;N](S)))
+
+Solution =  2x( SUM[M=1;N-1](M x SUM[S=M+1;N](S)))
+""")
+    Solution = 0
+    for M in range(1, max):
+        for S in range(M+1, max+1):
+            Solution = Solution + M*S
+    Solution = 2 * Solution
+
+    if difference != Solution:
+        print("'Brute force' implementation is different then 'Smart' implementation !!")
     
     print(40*"-")
-    print("Solution = {}".format(Solution))
+    print(f"Solution = {Solution}")
     print(40*"=")
 
 if __name__ == "__main__":
